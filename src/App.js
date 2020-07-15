fa307af296cc46c483ea9c3b10383029e5fdb306
Chayce SOlchaga <chayce.solchaga@gmail.com>
2020-07-15 16:40:06 -0700

    save point for module retro

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ import axios from 'axios'
 // import my API Key and url for api from Nasa via constants
 import { API_KEY, BASE_URL } from './constants'
 import "./App.css";
-
+import ApodPhoto from './components/ApodPhoto'
 
 
 function App() {
   
   //creating useState statement to have a place to set my data from the API into the DOM
-  const [photoState, setPhotoState] = useState({})
+  const [photo, setPhotoState] = useState({})
   
   //initiating useEffect to make a call to the API to get my data pulling in from constants. Helps to limit calls to API
   useEffect( () => {
-    axios.get(`${BASE_URL}/?api_key=${API_KEY}`)
+    axios.get(`${BASE_URL}/?api_key=${API_KEY}`) //&date=2020-07-13
     .then(res => {
       setPhotoState(res.data)
-      if (res.data.media_type !== "image") {
-        axios.get(`${BASE_URL}/?api_key=${API_KEY}&date=2012-03-14`)
-      } else {
-        axios.get(`${BASE_URL}/?api_key=${API_KEY}&date=2012-03-14`)
-      }
+      // if (res.data.media_type !== "image") {
+      //   axios.get(`${BASE_URL}/?api_key=${API_KEY}&date=2012-03-14`)
+      // } else {
+      //   axios.get(`${BASE_URL}/?api_key=${API_KEY}`)
+      // }
       
       console.log(res)
     })
@@ -32,18 +32,18 @@ function App() {
     })
   }, []); 
 
-  const ApodPhoto = ({ elm }) => (
-    <div className ="apod-div">
-      <div className ="apod-img-div">
-      <h2>Photo of the Day for {elm.date}</h2>
-        <img src={elm.url} alt={elm.title} />
-      </div>
-      <div className="apod-data">
-        <h2>{elm.title}</h2>
-        <p>{elm.explanation}</p>
-      </div>
-    </div>
-  )
+  // const ApodPhoto = ({ elm }) => (
+  //   <div className ="apod-div">
+  //     <div className ="apod-img-div">
+  //     <h2>Photo of the Day for {elm.date}</h2>
+  //       <img src={elm.url} alt={elm.title} />
+  //     </div>
+  //     <div className="apod-data">
+  //       <h2>{elm.title}</h2>
+  //       <p>{elm.explanation}</p>
+  //     </div>
+  //   </div>
+  // )
 
   return (
     <div className="App">
@@ -52,7 +52,7 @@ function App() {
       <a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos! </a>
       Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer!
       </p>
-      <ApodPhoto elm={photoState} />
+      {photo && <ApodPhoto photos={photo} />}
     </div>
   );
 }
--- src/components/ApodPhoto.js
+++ src/components/ApodPhoto.js
@@ -1,4 +1,20 @@
 import React from 'react'
 
+const ApodPhoto = props=> {
+    const {photo} = props
+  return(
+    <div className ="apod-div">
+    <div className ="apod-img-div">
+    <h2>Photo of the Day for {photo.date}</h2>
+      <img src={photo.url} alt={photo.title} />
+    </div>
+    <div className="apod-data">
+      <h2>{photo.title}</h2>
+      <p>{photo.explanation}</p>
+    </div>
+  </div>
+  )
+}
+
 
 export default ApodPhoto
