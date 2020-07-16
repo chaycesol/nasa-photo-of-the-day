a0ea0be6186aecaace9cf23b62e67dcbad90e9b3
Chayce SOlchaga <chayce.solchaga@gmail.com>
2020-07-16 11:18:03 -0700

    reinitiating into project cleaned up some code to make it easier to read

--- src/App.js
+++ src/App.js
@@ -12,19 +12,13 @@ import ApodPhoto from './components/ApodPhoto'
 function App() {
   
   //creating useState statement to have a place to set my data from the API into the DOM
-  const [photo, setPhotoState] = useState({})
+  const [currentPhoto, setCurrentPhoto] = useState({})
   
   //initiating useEffect to make a call to the API to get my data pulling in from constants. Helps to limit calls to API
   useEffect( () => {
     axios.get(`${BASE_URL}/?api_key=${API_KEY}`) //&date=2020-07-13
     .then(res => {
-      setPhotoState(res.data)
-      // if (res.data.media_type !== "image") {
-      //   axios.get(`${BASE_URL}/?api_key=${API_KEY}&date=2012-03-14`)
-      // } else {
-      //   axios.get(`${BASE_URL}/?api_key=${API_KEY}`)
-      // }
-      
+      setCurrentPhoto(res.data)
       console.log(res)
     })
     .catch(error => {
@@ -32,18 +26,6 @@ function App() {
     })
   }, []); 
 
-  // const ApodPhoto = ({ elm }) => (
-  //   <div className ="apod-div">
-  //     <div className ="apod-img-div">
-  //     <h2>Photo of the Day for {elm.date}</h2>
-  //       <img src={elm.url} alt={elm.title} />
-  //     </div>
-  //     <div className="apod-data">
-  //       <h2>{elm.title}</h2>
-  //       <p>{elm.explanation}</p>
-  //     </div>
-  //   </div>
-  // )
 
   return (
     <div className="App">
@@ -52,7 +34,7 @@ function App() {
       <a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos! </a>
       Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer!
       </p>
-      {photo && <ApodPhoto photos={photo} />}
+     <ApodPhoto date title explanation photo={currentPhoto} />}
     </div>
   );
 }
