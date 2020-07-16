a870e4e70efa3d843519852e0ea12d32e54432de
Chayce SOlchaga <chayce.solchaga@gmail.com>
2020-07-16 16:03:03 -0700

    Final stuff for styling

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import React, {useState, useEffect } from "react";
 import axios from 'axios'
 // import my API Key and url for api from Nasa via constants
 import { API_KEY, BASE_URL } from './constants'
+import styled from 'styled-components'
 import "./App.css";
 import ApodPhoto from './components/ApodPhoto'
 
+const StyledApp = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    background-image: url("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8374fac0-6f02-49a4-b287-125bf0580b45/d9eay0l-e6e11a40-1825-41c2-a649-493068ddc9fe.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvODM3NGZhYzAtNmYwMi00OWE0LWIyODctMTI1YmYwNTgwYjQ1XC9kOWVheTBsLWU2ZTExYTQwLTE4MjUtNDFjMi1hNjQ5LTQ5MzA2OGRkYzlmZS5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.tkQIqjeYoNsWDcj3Ycb1zS-QSFkOLxcy18wValps1eE");
+    background-repeat: no-repeat;
+    background-size: cover;
+    color: white;
+}
+    
+`
 
 function App() {
   
   //creating useState statement to have a place to set my data from the API into the DOM
   const [currentPhoto, setCurrentPhoto] = useState({})
+  const backupPhoto = ""
   
   //initiating useEffect to make a call to the API to get my data pulling in from constants. Helps to limit calls to API
   useEffect( () => {
-    axios.get(`${BASE_URL}/?api_key=${API_KEY}`) //&date=2020-07-13
+    axios.get(`${BASE_URL}/?api_key=${API_KEY}`) // copy and paste to test your api and if statement ->> &date=2020-07-13
     .then(res => {
       setCurrentPhoto(res.data)
       console.log(res)
@@ -26,16 +40,18 @@ function App() {
     })
   }, []); 
 
-
   return (
     <div className="App">
+      <StyledApp className = "nasaApp">
       <h1>Astronomy Photo of the Day! <span role="img" aria-label="space emojis">🚀🛰👽</span></h1> 
       <p>
       <a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos! </a>
       Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer!
       </p>
-     <ApodPhoto date media_type title explanation photo={currentPhoto} />}
-    </div>
+     <ApodPhoto date media_type title explanation photo={currentPhoto} />
+    </StyledApp>
+      </div>
+      
   );
 }
 
--- src/components/ApodPhoto.js
+++ src/components/ApodPhoto.js
@@ -1,27 +1,101 @@
 import React from 'react'
+import styled  from 'styled-components' //{ keyframes }
+
+// const kf = keyframes`
+//     100%{
+
+//     }
+//     50% {
+
+//     }
+//    100%{
+
+// }
+// `
+const StyledDetails = styled.div`
+
+    .apod-img-div{
+        position: relative;
+        width: 75%;
+        height: auto;
+        /* border: 2px dotted red; */
+        background-color: #000;
+
+
+    .image {
+        display: center;
+        width: 100%;
+        height: auto;
+        &:hover  {
+            opacity: 0.15;
+        }    
+    }
+    }
+
+    .overlay {
+        display: center;
+        /* border: 2px dotted yellow; */
+
+        height: 100%;
+        width: 100%;
+        opacity: 0;
+        /* transition: .5s ease; */
+        /* background-color: #000; */
+        
+        &:hover  {
+            opacity: 0.75;
+        }    
+    }
+
+    .apod-data {
+    color: #000;
+    /* border: 2px dotted red; */
+    font-size: 20px;
+    position: absolute;
+    top: 0.5%;
+    /* left: 50%; */
+    width: 100%;
+    height: 100%;
+    background-color: white;
+
+    /* transform: translate(-50%, -50%); */
+    text-align: center;
+}
+`
 
 const ApodPhoto = props=> {
     const {photo} = props
-    const backupPhoto = "https://i.ytimg.com/vi/3q-4YCxcc6U/maxresdefault.jpg"
-    if (photo.media_type === "image") {
-        photo.url = photo.url
-    } else if (photo.media_type !== "image"){
-        photo.url = backupPhoto
-    }
+    const backupPhoto = "https://i.imgur.com/V9WY8ga.png"
+
+            // 2nd fetch to check on media type
+        // logic to handle media type not being able to display video
+        if (photo.media_type === "image") {
+            photo.url = photo.url
+        } else if (photo.media_type !== "image"){
+            photo.url = backupPhoto
+            // check date and set date to minus day until it is an image
+            // newDate variable 
+            // const date = "datenow-1" 
+        }
 
   return(
-    <div className ="apod-div">
+    <StyledDetails className ="apod-div">
+        <h2>Photo of the Day for {photo.date}</h2>
     <div className ="apod-img-div">
-    <h2>Photo of the Day for {photo.date}</h2>
-      <img src={photo.url} alt={photo.title} />
+      <img className = "image" src={photo.url} alt={photo.title} />
+        <div className="overlay">
+            <div className="apod-data">
+                <h2>{photo.title}</h2>
+                <p>{photo.explanation}</p>
+            </div>
+        </div>
     </div>
-    <div className="apod-data">
-      <h2>{photo.title}</h2>
-      <p>{photo.explanation}</p>
-    </div>
-  </div>
+  </StyledDetails>
   )
 }
 
 
 export default ApodPhoto
+
+
+  
